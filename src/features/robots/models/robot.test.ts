4c6775b000c600c458ef5050560ea3ebb806a149
mireyachaparro <mireyacn99@gmail.com>
2022-11-19 18:00:51 +0100

    Add model test

---
+++ src/features/robots/models/robot.test.ts
@@ -0,0 +1,10 @@
+import { RobotModel } from './robot';
+
+describe('Given the class RobotModel', () => {
+    describe('When we instantiate it', () => {
+        const task = new RobotModel('', '', 1, 1, '');
+        test('Then we should have an object ot the class', () => {
+            expect(task).toBeInstanceOf(RobotModel);
+        });
+    });
+});
