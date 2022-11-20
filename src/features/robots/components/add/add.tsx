9e80bea14d61a81b3d3f93680318eb0df73306b6
mireyachaparro <mireyacn99@gmail.com>
2022-11-20 13:22:56 +0100

    Add components and tests

---
+++ src/features/robots/components/add/add.tsx
@@ -0,0 +1,94 @@
+import { SyntheticEvent, useState } from 'react';
+import { useRobots } from '../../hooks/use.robots';
+import { ProtoRobot } from '../../models/robot';
+
+type formData = {
+    name: string;
+    img: string;
+    speed: number;
+    resistance: number;
+    date: string;
+};
+export function Add() {
+    const title = 'Añadir robot';
+    const initialState: formData = {
+        name: 'prueba',
+        img: '123.jpg',
+        speed: 1,
+        resistance: 2,
+        date: '2000',
+    };
+    const [formState, setFormState] = useState(initialState);
+    const { handleAdd } = useRobots();
+
+    const handleInput = (ev: SyntheticEvent) => {
+        const element = ev.target as HTMLFormElement;
+        setFormState({ ...formState, [element.name]: element.value });
+    };
+
+    const handleSubmit = (ev: SyntheticEvent) => {
+        ev.preventDefault();
+        const newRobot: ProtoRobot = { ...formState };
+        handleAdd(newRobot);
+    };
+
+    return (
+        <>
+            <h2>{title}</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        name="name"
+                        aria-label="Name"
+                        placeholder="Name"
+                        value={formState.name}
+                        onInput={handleInput}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="img"
+                        aria-label="Image"
+                        placeholder="Imagen"
+                        value={formState.img}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="speed"
+                        aria-label="Speed"
+                        placeholder="Speed"
+                        value={formState.speed}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="resistance"
+                        aria-label="Resistance"
+                        placeholder="Resistance"
+                        value={formState.resistance}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="date"
+                        aria-label="Date"
+                        placeholder="Date of creation"
+                        value={formState.date}
+                        onInput={handleInput}
+                    />
+                </div>
+                <button type="submit">Guardar</button>
+            </form>
+        </>
+    );
+}
--- src/infrastructure/components/routes/app.routes.tsx
+++ src/infrastructure/components/routes/app.routes.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
-const Home = lazy(() => import('../../../features/home/page/home.page'));
+// const Home = lazy(() => import('../../../features/home/page/home.page'));
+const Robots = lazy(() => import('../../../features/robots/page/robots.page'));
 export function AppRoutes() {
     return (
         <Suspense>
             <Routes>
-                <Route path="home" element={<Home></Home>}></Route>
+                <Route path="home" element={<Robots></Robots>}></Route>
             </Routes>
         </Suspense>
     );
